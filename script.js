24af5ec060f51e0eb957125a3c215c344377973a
Tania Terence <ainatoa2@gmail.com>
2024-07-28 16:34:24 -0300

    two separate hide/show toggle functions for dateBox and checkMark

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-let dateBox = document.getElementsByClassName(".grid-item")
-let checkMark = document.querySelectorAll(".check-mark")
-console.log(dateBox)
-console.log(checkMark)
+$(function () {
+  let dateBox = document.getElementsByClassName("grid-item");
+  let checkMark = document.getElementsByClassName("check-mark");
 
-for (let i = 0; i < dateBox.length; i++) {
-    dateBox[i].addEventListener("click", (event) => {
-        console.log("I've been clicked!")
-        event.target.closest().style.visibility = "visible"
-    });
-}
+  console.log(dateBox[0]);
+  console.log(checkMark[0]);
 
+
+  function toggleHideShowBox(event) {
+    console.log(event)
+    console.log("I've been clicked!");
+      //console.log(event.target.childNodes[1]);
+      if(event.target.childNodes[1].style.visibility == "hidden") {
+        event.target.childNodes[1].style.visibility = "visible"
+      }
+      else {
+        event.target.childNodes[1].style.visibility = "hidden"
+      }
+  }
+
+  function toggleHideShowCheck(event) {
+    console.log(event)
+    console.log("I've been clicked!");
+      //console.log(event.target.childNodes[1]);
+      if(event.target.style.visibility == "hidden") {
+        event.target.style.visibility = "visible"
+      }
+      else {
+        event.target.style.visibility = "hidden"
+      }
+  }
+
+  for (let i = 0; i < dateBox.length; i++) {
+    dateBox[i].addEventListener("click", toggleHideShowBox);
+    checkMark[i].addEventListener("click", toggleHideShowCheck);
+  }
+})
